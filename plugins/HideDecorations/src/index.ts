0128a9968867d46a0f043f5943241c921bee671f
pokemonrocks9 <tytygoins@gmail.com>
2025-11-28 08:41:37 -0500

    Update index.ts
    Remove nameplates

--- plugins/HideDecorations/src/index.ts
+++ plugins/HideDecorations/src/index.ts
@@ -6,28 +6,33 @@ let patches: (() => void)[] = [];
 export default {
     onLoad: () => {
         try {
-            // TEST 2: Patch UserStore.getUser
             const UserStore = findByStoreName('UserStore');
             if (UserStore?.getUser) {
                 patches.push(
                     instead('getUser', UserStore, (args, orig) => {
                         const user = orig(...args);
                         if (user) {
+                            // Hide avatar decorations
                             user.avatarDecoration = null;
                             user.avatarDecorationData = null;
+                            
+                            // Hide profile effects
+                            user.profileEffectId = null;
+                            
+                            // Hide nameplates (the glowing highlight)
+                            user.nameplateId = null;
                         }
                         return user;
                     })
                 );
-                console.log("[HideDecorations] Test 2: UserStore.getUser patched");
+                console.log("[HideDecorations] UserStore.getUser patched - hiding decorations, effects, and nameplates");
             } else {
-                console.log("[HideDecorations] Test 2: UserStore.getUser NOT found");
+                console.log("[HideDecorations] UserStore.getUser NOT found");
             }
         } catch (error) {
             console.error("[HideDecorations] Error:", error);
         }
     },
-
     onUnload: () => {
         patches.forEach((unpatch) => unpatch());
         patches = [];
