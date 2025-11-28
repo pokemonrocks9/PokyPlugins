d15fb7efa97f495be4fcdc88e1aadb1d462dd8cc
pokemonrocks9 <tytygoins@gmail.com>
2025-11-28 08:44:16 -0500

    Update index.ts

--- plugins/HideDecorations/src/index.ts
+++ plugins/HideDecorations/src/index.ts
@@ -1,42 +1,22 @@
-import { findByStoreName, findByProps } from '@vendetta/metro';
-import { instead, before } from '@vendetta/patcher';
+import { findByStoreName } from '@vendetta/metro';
+import { instead } from '@vendetta/patcher';
 
 let patches: (() => void)[] = [];
 
 export default {
     onLoad: () => {
         try {
-            // Patch 1: UserStore.getUser
             const UserStore = findByStoreName('UserStore');
             if (UserStore?.getUser) {
                 patches.push(
                     instead('getUser', UserStore, (args, orig) => {
                         const user = orig(...args);
                         if (user) {
-                            // Hide avatar decorations
-                            user.avatarDecoration = null;
-                            user.avatarDecorationData = null;
+                            // LOG ALL PROPERTIES to find the nameplate one
+                            console.log("[HideDecorations] User object keys:", Object.keys(user));
+                            console.log("[HideDecorations] User object:", JSON.stringify(user, null, 2));
                             
-                            // Hide profile effects
-                            user.profileEffectId = null;
-                            
-                            // Hide nameplates - try all possible property names
-                            user.nameplateId = null;
-                            user.nameplate = null;
-                            user.nameplateData = null;
-                        }
-                        return user;
-                    })
-                );
-                console.log("[HideDecorations] UserStore.getUser patched");
-            }
-
-            // Patch 2: Try to find and patch the getCurrentUser method too
-            if (UserStore?.getCurrentUser) {
-                patches.push(
-                    instead('getCurrentUser', UserStore, (args, orig) => {
-                        const user = orig(...args);
-                        if (user) {
+                            // Hide everything we know about
                             user.avatarDecoration = null;
                             user.avatarDecorationData = null;
                             user.profileEffectId = null;
@@ -47,35 +27,8 @@ export default {
                         return user;
                     })
                 );
-                console.log("[HideDecorations] UserStore.getCurrentUser patched");
+                console.log("[HideDecorations] Debug mode active - check console for user object properties");
             }
-
-            // Patch 3: Try to find the module that handles nameplate rendering
-            // This might be named something like "getNameplateForUser" or similar
-            const NameplateModule = findByProps('getNameplateForUser');
-            if (NameplateModule?.getNameplateForUser) {
-                patches.push(
-                    instead('getNameplateForUser', NameplateModule, () => null)
-                );
-                console.log("[HideDecorations] Nameplate module patched");
-            }
-
-            // Patch 4: Alternative - look for getUserProfile which might contain this data
-            const UserProfileStore = findByStoreName('UserProfileStore');
-            if (UserProfileStore?.getUserProfile) {
-                patches.push(
-                    instead('getUserProfile', UserProfileStore, (args, orig) => {
-                        const profile = orig(...args);
-                        if (profile) {
-                            profile.profileEffectId = null;
-                            profile.nameplateId = null;
-                        }
-                        return profile;
-                    })
-                );
-                console.log("[HideDecorations] UserProfileStore.getUserProfile patched");
-            }
-
         } catch (error) {
             console.error("[HideDecorations] Error:", error);
         }
