c00eed88edfbb3e35563af304daa7af70aafad3f
pokemonrocks9 <tytygoins@gmail.com>
2026-04-13 11:42:45 -0400

    Update index.ts
    Updated fix

--- plugins/HideDecorations/src/index.ts
+++ plugins/HideDecorations/src/index.ts
@@ -1,25 +1,37 @@
 import { findByStoreName } from '@vendetta/metro';
 import { instead } from '@vendetta/patcher';
+
+const normalizeFonts = (text: string) => 
+    text.normalize("NFKD").replace(/[\u0300-\u036f]/g, "");
+
 let patches: (() => void)[] = [];
+
 export default {
     onLoad: () => {
         try {
-            // TEST 2: Patch UserStore.getUser
             const UserStore = findByStoreName('UserStore');
+
             if (UserStore?.getUser) {
                 patches.push(
                     instead('getUser', UserStore, (args, orig) => {
                         const user = orig(...args);
                         if (user) {
+                            // Hide avatar decorations and frames
                             user.avatarDecoration = null;
                             user.avatarDecorationData = null;
+
+                            // Hide profile effects (animations) and nameplates
+                            user.profileEffectId = null;
+                            // @ts-ignore - nameplate is a newer field in the Discord User object
+                            user.nameplate = null;
+
+                            // "Fix" fonts by normalizing stylized unicode in display names
+                            // We do not modify user.clan, so clan tags remain untouched
+                            if (user.globalName) user.globalName = normalizeFonts(user.globalName);
                         }
                         return user;
                     })
                 );
-                console.log("[HideDecorations] Test 2: UserStore.getUser patched");
-            } else {
-                console.log("[HideDecorations] Test 2: UserStore.getUser NOT found");
             }
         } catch (error) {
             console.error("[HideDecorations] Error:", error);
